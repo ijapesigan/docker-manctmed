d6e187f9c1c15140469bfa6864663679e21e145e
ijapesigan <ijapesigan@gmail.com>
2024-03-19 05:31:23 +0000

    Automated build.

--- scripts/simstatespace.sh
+++ scripts/simstatespace.sh
@@ -6,6 +6,6 @@ R -e "install.packages('simStateSpace')"
 
 R -e "remotes::install_version(                                \
     package = 'simStateSpace',                                 \
-    version = '1.2.1',                                         \
+    version = '1.2.0',                                         \
     repos = 'https://packagemanager.rstudio.com/all/latest'    \
 )"
